91ff628b12d6e63e55ce0221bd20415950f8e4aa
Mark Pinero <markspinero@gmail.com>
2017-03-30 02:41:59 +0000

    add new block tests

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
 import React, { Component } from 'react';
+import Block from './components/Block';
+import BlockContainer from './components/BlockContainer';
+
+const map = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 1]
+map = map.concat(...map);
+map = map.concat(...map);
 
 class App extends Component {
+  
   render() {
+    const bloc = map.map((e, i) => {
+      return (
+        <Block key={i} data={e} />
+        )
+    });
+
     return (
       <div className="App">
-        <div className="App-header">
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        {bloc}
       </div>
     );
   }
