530b764cbfef59951bb78f1bb8d9967dc86a9e7c
remarks <markspinero@gmail.com>
2017-04-26 01:23:16 -0700

    grid working. WIP styling

--- client/src/components/EventGrid/EventGridContainer.js
+++ client/src/components/EventGrid/EventGridContainer.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import { Segment } from 'semantic-ui-react';
 import { connect } from 'react-redux';
-import EventsList from './EventsList';
 import * as apiActions from '../../actions/apiActions';
+import moment from 'moment';
+import _ from 'lodash';
+import Box from './Box';
+
+import './styles.css';
 
 class EventGridContainer extends React.Component {
   componentDidMount() {
@@ -10,13 +14,54 @@ class EventGridContainer extends React.Component {
   }
 
   render() {
+    const data = [];
+    const dob = moment(this.props.user.dateOfBirth).add(1, 'day');
+    const weeksAlive = moment().diff(dob, 'weeks');
+    const mm = moment(dob).get('month');
+    const dd = moment(dob).add(1, 'day').get('date');
+    console.log(dd);
+
+    for (let week = 0; week <= weeksAlive; week++) {
+      let weekStart = moment(dob).add(week, 'weeks').format('YYYY-MM-DD');
+      let weekEnd = moment(weekStart).add(6, 'days').format('YYYY-MM-DD');
+      let yy = moment(weekStart).get('year');
+
+      let thisWeeksEvents = [];
+      thisWeeksEvents = this.props.events.filter((event, index) => {
+        if (moment(event.date).isBetween(weekStart, weekEnd, null, '[]')) {
+          return event;
+        } else {
+          return null;
+        }
+      });
+
+      let birthday = false;
+      if (moment([yy, mm, dd]).isBetween(weekStart, weekEnd, null, '[]')) {
+        birthday = true;
+      }
+
+      data.push(
+        <Box
+          key={week}
+          weekStart={weekStart}
+          events={thisWeeksEvents}
+          birthday={birthday}
+        />
+      );
+    }
+
     return (
       <Segment>
         <h1>Timeline</h1>
-        <EventsList />
+        <div className="clear">{data}</div>
       </Segment>
     );
   }
 }
 
-export default connect()(EventGridContainer);
+const mapStateToProps = state => ({
+  events: state.api.events,
+  user: state.api.user
+});
+
+export default connect(mapStateToProps)(EventGridContainer);
