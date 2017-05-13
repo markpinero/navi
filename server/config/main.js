af810113d3f973bf283eccca9b954da7d36954d8
remarks <markspinero@gmail.com>
2017-05-13 13:22:15 -0700

    adjust config

--- server/config/main.js
+++ server/config/main.js
@@ -1,5 +1,5 @@
 module.exports = {
   port: process.env.PORT || 3001,
-  secret: "GGXyBzdL#W2cGHEuphkYwTre",
-  database: "mongodb://remarks:ZKv1iHk5eoTQhZKk@navi-shard-00-00-zk9jh.mongodb.net:27017,navi-shard-00-01-zk9jh.mongodb.net:27017,navi-shard-00-02-zk9jh.mongodb.net:27017/Navi?ssl=true&replicaSet=Navi-shard-0&authSource=admin"
+  secret: process.env.SECRET,
+  database: process.env.DATABASE_URL
 };
