2c41be07f2f3f4a82c2ececd3098cf4203eae222
remarks <markspinero@gmail.com>
2017-05-13 13:06:27 -0700

    remove server-side code for client branch

--- server/config/main.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  port: process.env.PORT || 3001,
-  secret: "GGXyBzdL#W2cGHEuphkYwTre",
-  database: "mongodb://remarks:ZKv1iHk5eoTQhZKk@navi-shard-00-00-zk9jh.mongodb.net:27017,navi-shard-00-01-zk9jh.mongodb.net:27017,navi-shard-00-02-zk9jh.mongodb.net:27017/Navi?ssl=true&replicaSet=Navi-shard-0&authSource=admin"
-};
--- start-server.js
+++
@@ -1,5 +0,0 @@
-const app = require("./server");
-
-app.listen(app.get("port"), () => {
-  console.log(`Find the server at: http://localhost:${app.get("port")}/`); // eslint-disable-line no-console
-});
