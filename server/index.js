16499dac93e154ddecf3fa73c5d3ccb9bad19bb2
Mark Pinero <markspinero@gmail.com>
2018-01-06 23:59:42 -0800

    add root response

--- server/index.js
+++ server/index.js
@@ -1,25 +1,29 @@
-const express = require('express');
-const cors = require('cors');
-const logger = require('morgan');
-const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
-const passport = require('passport');
-const config = require('./config/main');
+const express = require('express')
+const cors = require('cors')
+const logger = require('morgan')
+const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
+const passport = require('passport')
+const config = require('./config/main')
 
-const app = express();
+const app = express()
 
-mongoose.connect(config.database);
-mongoose.Promise = global.Promise;
+mongoose.connect(config.database)
+mongoose.Promise = global.Promise
 
-app.use(logger('dev'));
-app.use(cors());
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(passport.initialize());
+app.use(logger('dev'))
+app.use(cors())
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(passport.initialize())
 
-const router = require('./router');
-router(app);
+const router = require('./router')
+router(app)
 
-app.set('port', config.port);
+app.get('/', (req, res) => {
+  res.status(200).json({ message: 'OK' })
+})
 
-module.exports = app;
+app.set('port', config.port)
+
+module.exports = app
